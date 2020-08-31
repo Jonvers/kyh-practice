95ab83c4e808e12d1afc151e676370dc744bfdca
Jonvers <jonathan.werner@hotmail.com>
2020-08-31 11:33:00 +0200

    Add files via upload
    Added first code

---
+++ Introduction.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_introduction():
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hej. Mitt namn är Jonathan Vernersson och jag har växt upp i Hålanda. Jag har tidigare studerat objektorienterad systemutveckling.'
+          f' \nDå har jag fokuserat på språken Java och C#, men även lärt mig mer frontendfokuserade språk som Javascript och Typescript.'
+          f'\nJag sökte denna utbildning för jag vill arbeta med mjukvarutestning')
+
+
+
+if __name__ == '__main__':
+    print_introduction()
+
