ca68c28fbb9e5c719971532c251b88da30b20197
Jonathan Vernersson <jonathan.vernersson@student.kyh.se>
2020-09-01 11:10:08 +0200

    Added guess number loop in a function

--- Guess number.py
+++ Guess number.py
@@ -1,20 +1,28 @@
 import random
 
+
 n = random.randint(1, 100)
 print("Jag tänker på ett nummer mellan 1 och 100. Gissa vilken?")
 
-guess = 0
-while True:
-    text = input("Din gissning: ")
-    as_number = int(text)
-    guess = guess + 1
 
-    if as_number == n:
-        print("Korrekt! Du har gjort " + str(guess) + " gissningar")
-        break
 
-    if as_number < n:
-        print("Fel, mitt nummer är högre... Försök igen!")
 
-    if as_number > n:
-        print("Fel, mitt nummer är lägre... Försök igen!")
+def mainloop():
+    guess = 0
+    while True:
+        text = input("Din gissning: ")
+        as_number = int(text)
+        guess = guess + 1
+
+        if as_number == n:
+            print("Korrekt!")
+            return guess
+
+        if as_number < n:
+            print("Fel, mitt nummer är högre... Försök igen!")
+
+        if as_number > n:
+            print("Fel, mitt nummer är lägre... Försök igen!")
+
+guess = str(mainloop())
+print("Du har gissat " + guess + " gånger")
