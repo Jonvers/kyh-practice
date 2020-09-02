135c9cb5be386c3ea27d96ede8b7bde21db30d19
Jonathan Vernersson <jonathan.vernersson@student.kyh.se>
2020-09-02 09:31:58 +0200

    Fixed multiply and divide function

--- CalculateNumbers.py
+++ CalculateNumbers.py
@@ -8,17 +8,19 @@ def subtract(a, b):
     return result
 
 def multiply(a, b):
-    result = a + b
+    result = a * b
     return result
 
 def divide(a, b):
-    result = a + b
+    result = a / b
     return result
 
 def run():
     print("This program will help with simple calculations. What do you want to do?")
     print("1 - add numbers")
     print("2 - subtract numbers")
+    print("3 - multiply numbers")
+    print("4 - divide numbers")
     answer = input(">> ")
     a = int(input("A="))
     b = int(input("B="))
@@ -28,6 +30,12 @@ def run():
 
     if answer == "2":
         result = subtract(a, b)
+
+    if answer == "3":
+        result = multiply(a, b)
+
+    if answer == "4":
+        result = divide(a, b)
 
     print("Result = " + str(result))
 
