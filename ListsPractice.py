77880fbad21e6834a1d8c33cc22996f7ce4bf797
Jonathan Vernersson <jonathan.vernersson@student.kyh.se>
2020-09-07 14:48:35 +0200

    Added listsPractice function and module

---
+++ ListsPractice.py
@@ -0,0 +1,19 @@
+def is_it_too_long(name, number):
+    return len(name) > number
+
+def main():
+    try:
+        number = int(input("Hur långt får namnet vara?"))
+
+    except ValueError:
+        print("Olagligt nummer, sätter den tillåtna längden till 5")
+        number = 5
+
+    students = input("Skriv personernas namn").split(',')
+
+    for name in students:
+        if is_it_too_long(name, number):
+            print(f"{name.title()} är för långt!")
+
+if __name__ == '__main__':
+    main()
