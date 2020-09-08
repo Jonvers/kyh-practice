965431a907297ebadea22ca1f93e7277139c463e
Jonathan Vernersson <jonathan.vernersson@student.kyh.se>
2020-09-08 08:58:44 +0200

    Added groupLists in change_word.py

--- change_word.py
+++ change_word.py
@@ -6,13 +6,14 @@ def main():
     mobiltelefonen = ["en dator", "en bil", "en tvättmaskin", "ett par google glasses"]
     coronaviruset = ["zombieviruset", "magsjuka", "löss", "scenskräck"]
     sverige = ["Narnia", "Midgård", "Norge", "Finland"]
-    markku_tervahauta = ["Peter Forsberg", "Kattla", "Darth Vader", "Markoolio"]
+    markku_tervahauta = ["Peter Forsberg", "Katla", "Darth Vader", "Markoolio"]
     en_app = ["Ett dokument", "Ett hologram", "En stenmålning", "Ett datorspel"]
     chef = ["Sith lord", "Styvfar", "Gudfadern", "Bambatant"]
+    gruppen = ["Dödsätarna, DÄ", "Svenska fotbollsförbundet, SvFF", "Anonyma alkolisterna, AA", "Din mamma, DM"]
 
     print((f"""{en_app[random.randint(0,3)]} som kan laddas ned till {mobiltelefonen[random.randint(0, 3)]} ska varna finländare som kommit nära någon som smittats med {coronaviruset[random.randint(0, 3)]}.
 – Jag tycker att ni i {sverige[random.randint(0, 3)]} borde överväga att göra något liknande, säger {markku_tervahauta[random.randint(0, 3)]}, {chef[random.randint(0, 3)]} för
-Finska institutet för hälsa och välfärd, THL."""))
+{gruppen[random.randint(0,3)]}."""))
 
 
 if __name__ == '__main__':
