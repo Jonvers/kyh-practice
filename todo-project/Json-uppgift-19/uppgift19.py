f1c452be53c35c72d7535fa1b9dcd94a5f924956
Jonathan Vernersson <jonathan.vernersson@student.kyh.se>
2020-09-13 10:45:12 +0200

    Added uppgift 19 python code with accompanying json file

---
+++ todo-project/Json-uppgift-19/uppgift19.py
@@ -0,0 +1,12 @@
+import json
+from pathlib import Path
+
+def main():
+    content = Path("data.json").read_text(encoding="utf8")
+    data = json.loads(content)
+    pass
+
+# int, float, bool, string, dictionary
+
+if __name__ == '__main__':
+    main()
